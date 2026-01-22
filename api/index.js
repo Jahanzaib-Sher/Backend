564b2a0f15a416ab9a36cc27b1153c8392f3f68c
ubaidallahazam-cyber <ubaidallahazam@gmail.com>
2026-01-23 01:34:45 +0500

    update

--- api/index.js
+++ api/index.js
@@ -17,5 +17,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API is running 🚀");
+});
 
 module.exports = app;
